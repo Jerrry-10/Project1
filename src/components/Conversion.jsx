059ad8fbf2559202c2e96ceb5bb374370e00f34e
Jerrry-10 <jerryaviles10@gmail.com>
2023-03-08 10:07:06 -0500

    adds conversion page

--- src/components/Conversion.jsx
+++ src/components/Conversion.jsx
@@ -1,7 +1,121 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 
-function Conversion() {
-  return <div style={{ marginTop: "70px" }}>HOLA!!</div>;
-}
-export default Conversion;
+const Conversions = () => {
+  const [currency, setCurrency] = useState("USD");
+  const [amount, setAmount] = useState(0);
+  const [rates, setRates] = useState([]);
+  const [btcValue, setBtcValue] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => {
+        setRates(response.data.bpi);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleCurrencyChange = (e) => {
+    setCurrency(e.target.value);
+  };
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+    const rate = rates[currency].rate_float;
+    const value = e.target.value / rate;
+    setBtcValue(value);
+  };
+
+  const handleSortAscending = () => {
+    const sortedRates = Object.entries(rates).sort(
+      (a, b) =>
+        a[1].rate_float / rates[currency].rate_float -
+        b[1].rate_float / rates[currency].rate_float
+    );
+    setRates(Object.fromEntries(sortedRates));
+  };
+
+  const handleSortDescending = () => {
+    const sortedRates = Object.entries(rates).sort(
+      (a, b) =>
+        b[1].rate_float / rates[currency].rate_float -
+        a[1].rate_float / rates[currency].rate_float
+    );
+    setRates(Object.fromEntries(sortedRates));
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 2,
+        marginTop: "75px",
+      }}
+    >
+      <Typography variant="h5">Conversions</Typography>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <Button variant="contained" onClick={handleSortAscending}>
+          Sort Ascending
+        </Button>
+        <Button variant="contained" onClick={handleSortDescending}>
+          Sort Descending
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 1,
+        }}
+      >
+        {Object.entries(rates).map(([currency, rate]) => (
+          <Typography key={currency}>
+            {currency}: {rate.rate_float} = 1 BTC
+          </Typography>
+        ))}
+      </Box>
+      <FormControl variant="outlined">
+        <InputLabel id="currency-select-label">Currency</InputLabel>
+        <Select
+          labelId="currency-select-label"
+          id="currency-select"
+          value={currency}
+          onChange={handleCurrencyChange}
+          label="Currency"
+        >
+          <MenuItem value="USD">USD</MenuItem>
+          <MenuItem value="EUR">EUR</MenuItem>
+          <MenuItem value="GBP">GBP</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField
+        id="outlined-basic"
+        label="Amount"
+        variant="outlined"
+        value={amount}
+        onChange={handleAmountChange}
+      />
+      <Typography variant="h6">
+        {amount} {currency} = {btcValue} BTC
+      </Typography>
+    </Box>
+  );
+};
+
+export default Conversions;
--- src/components/CurrentConversionRate.jsx
+++ src/components/CurrentConversionRate.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Box, Button } from "@mui/material";
 import CurrencyPoundIcon from "@mui/icons-material/CurrencyPound";
 import EuroIcon from "@mui/icons-material/Euro";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
@@ -39,62 +40,66 @@ function CurrentConversionRate() {
     }
   };
   return (
-    <div style={{ display: "flex", marginTop: "70px" }}>
-      <div style={{ flex: 1 }}>
+    <Box sx={{ display: "flex", marginTop: "70px" }}>
+      <Box sx={{ flex: 1 }}>
         <p>Disclaimer: {currentPrice.disclaimer} </p>
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           <AttachMoneyIcon />
           <p style={{ marginLeft: "5px" }}>
             USD: {currentPrice.bpi?.USD.rate_float} = 1 Bitcoin
           </p>{" "}
           <CurrencyBitcoinIcon />
-        </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           <CurrencyPoundIcon />
           <p style={{ marginLeft: "5px" }}>
             GBP: {currentPrice.bpi?.GBP.rate_float} = 1 Bitcoin
           </p>{" "}
           <CurrencyBitcoinIcon />
-        </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           <EuroIcon />
           <p style={{ marginLeft: "5px" }}>
             EUR: {currentPrice.bpi?.EUR.rate_float} = 1 Bitcoin
           </p>{" "}
           <CurrencyBitcoinIcon />
-        </div>
+        </Box>
         <p>
           {" "}
           Time updated: {new Date(currentPrice.time?.updated).toLocaleString()}
         </p>
-        <button onClick={handleRefresh} disabled={isRefreshing}>
+        <Button
+          variant="contained"
+          onClick={handleRefresh}
+          disabled={isRefreshing}
+        >
           {isRefreshing ? "Refreshing..." : "Refresh"}
-        </button>
-      </div>
-      <div style={{ flex: 1, marginTop: "70px" }}>
-        <div style={{ display: "flex", alignItems: "center" }}>
+        </Button>
+      </Box>
+      <Box sx={{ flex: 1, marginTop: "70px" }}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           <AttachMoneyIcon />
           <p style={{ marginLeft: "5px" }}>
             1 USD = {1 / currentPrice.bpi?.USD.rate_float} Bitcoin
           </p>{" "}
           <CurrencyBitcoinIcon />
-        </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           <CurrencyPoundIcon />
           <p style={{ marginLeft: "5px" }}>
             1 GBP = {1 / currentPrice.bpi?.GBP.rate_float} Bitcoin
           </p>{" "}
           <CurrencyBitcoinIcon />
-        </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           <EuroIcon />
           <p style={{ marginLeft: "5px" }}>
             1 EUR = {1 / currentPrice.bpi?.EUR.rate_float} Bitcoin
           </p>{" "}
           <CurrencyBitcoinIcon />
-        </div>
-      </div>
-    </div>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
